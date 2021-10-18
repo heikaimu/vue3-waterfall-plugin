9bd7d67a44a18ac7e98ca3ee6053b2f5a8958b5e
Liu YaoWen <576079353@qq.com>
2021-10-18 17:33:15 +0800

    preivew

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,23 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    // assetsDir: './',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'VueWaterfallPluginNext',
-      fileName: format => `vue-waterfall-plugin-next.${format}.js`
-    },
-    rollupOptions: {
-      // 不想被打包进来的依赖
-      external: [
-        'vue'
-      ],
-      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-      output: {
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
+    base:'/vue3-waterfall-plugin/',
+    // lib: {
+    //   entry: path.resolve(__dirname, 'src/index.js'),
+    //   name: 'VueWaterfallPluginNext',
+    //   fileName: format => `vue-waterfall-plugin-next.${format}.js`
+    // },
+    // rollupOptions: {
+    //   // 不想被打包进来的依赖
+    //   external: [
+    //     'vue'
+    //   ],
+    //   // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+    //   output: {
+    //     globals: {
+    //       vue: 'Vue'
+    //     }
+    //   }
+    // }
   }
 })
