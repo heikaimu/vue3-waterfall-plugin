f71015c8efe725483b90111bb54303218f6e9ea7
Liu YaoWen <576079353@qq.com>
2022-03-18 15:33:40 +0800

    chore: 修改example的打包脚本

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: Yaowen Liu
  * @Date: 2022-03-11 16:26:27
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2022-03-17 13:50:13
+ * @LastEditTime: 2022-03-18 15:32:20
  */
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -11,12 +11,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   let buildConfig = {}
-  if (mode === 'example') {
-    buildConfig = {
-      outDir: 'example',
-    }
-  }
-  else if (mode === 'lib') {
+  if (mode === 'lib') {
     buildConfig = {
       lib: {
         entry: path.resolve(__dirname, 'src/index.ts'),
@@ -33,6 +28,11 @@ export default defineConfig(({ mode }) => {
           },
         },
       },
+    }
+  }
+  else {
+    buildConfig = {
+      outDir: 'example',
     }
   }
   return {
