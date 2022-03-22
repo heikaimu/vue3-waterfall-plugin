97135201ca9ada9d8598175929599dacda4c3648
Liu YaoWen <576079353@qq.com>
2022-03-22 09:55:01 +0800

    chore: build修改

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: Yaowen Liu
  * @Date: 2022-03-11 16:26:27
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2022-03-21 18:21:41
+ * @LastEditTime: 2022-03-22 09:51:43
  */
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -11,24 +11,24 @@ import vue from '@vitejs/plugin-vue'
 const target = process.env.TARGET
 
 let buildConfig: Record<string, unknown> = {
-  lib: {
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    name: 'MyLib',
-    fileName: (format: any) => `my-lib.${format}.js`,
-  },
-  rollupOptions: {
-    external: ['vue'],
-    output: {
-      globals: {
-        vue: 'Vue',
-      },
-    },
-  },
+  outDir: 'netlify-page',
 }
 
-if (target === 'page') {
+if (target === 'npm') {
   buildConfig = {
-    outDir: 'example',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyLib',
+      fileName: (format: any) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   }
 }
 
