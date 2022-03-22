479400436bc99bf6ada864bbce44645a462d3b6d
Liu YaoWen <576079353@qq.com>
2022-03-22 09:42:59 +0800

    chore: build修改

--- vite.config.ts
+++ vite.config.ts
@@ -2,49 +2,40 @@
  * @Author: Yaowen Liu
  * @Date: 2022-03-11 16:26:27
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2022-03-18 15:32:20
+ * @LastEditTime: 2022-03-21 18:21:41
  */
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  let buildConfig = {}
-  if (mode === 'lib') {
-    buildConfig = {
-      lib: {
-        entry: path.resolve(__dirname, 'src/index.ts'),
-        name: 'MyLib',
-        fileName: (format: any) => `my-lib.${format}.js`,
-      },
-      rollupOptions: {
-        // 确保外部化处理那些你不想打包进库的依赖
-        external: ['vue'],
-        output: {
-          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-          globals: {
-            vue: 'Vue',
-          },
-        },
-      },
-    }
-  }
-  else {
-    buildConfig = {
-      outDir: 'example',
-    }
-  }
-  return {
-    build: buildConfig,
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`,
+const target = process.env.TARGET
+
+let buildConfig: Record<string, unknown> = {
+  lib: {
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    name: 'MyLib',
+    fileName: (format: any) => `my-lib.${format}.js`,
+  },
+  rollupOptions: {
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue',
       },
     },
-    plugins: [vue()],
-    server: {
-      open: true,
-    },
+  },
+}
+
+if (target === 'page') {
+  buildConfig = {
+    outDir: 'example',
   }
+}
+
+export default defineConfig({
+  build: { ...buildConfig },
+  plugins: [vue()],
+  server: {
+    open: true,
+  },
 })
