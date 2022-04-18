bb8a9580cc9ad07066e120df14fac0151e991b54
Liu YaoWen <576079353@qq.com>
2022-04-18 13:56:53 +0800

    chore: 掘金简单demo

--- example/App.vue
+++ example/App.vue
@@ -2,12 +2,13 @@
  * @Author: Yaowen Liu
  * @Date: 2022-03-11 16:26:27
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2022-03-24 14:24:31
+ * @LastEditTime: 2022-04-18 10:38:55
 -->
 <template>
   <WaterfallApp />
 </template>
 
 <script setup lang="ts">
-import WaterfallApp from './components/WaterfallApp.vue'
+// import WaterfallApp from './components/WaterfallApp.vue'
+import WaterfallApp from './components/Simple.vue'
 </script>
