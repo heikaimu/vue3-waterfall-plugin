680c71ee2f2bf9730e7ff90d12a9727cf9440848
Liu YaoWen <576079353@qq.com>
2024-08-27 09:43:35 +0800

    chore: 修改插件以来包，剔除所有example用的依赖

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,15 @@ let buildConfig: Record<string, unknown> = {
 if (target === 'npm') {
   buildConfig = {
     lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'MyLib',
-      fileName: (format: any) => `my-lib.${format}.js`,
+      entry: path.resolve(__dirname, 'lib/index.ts'), // 入口文件
+      name: 'MyLib', // 库的全局变量名称
+      fileName: (format: any) => `my-lib.${format}.js`, // 输出文件名
     },
     rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
